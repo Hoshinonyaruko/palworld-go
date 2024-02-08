27bd2a8e05ddc0866846af3e0efb37f34f5a1176
SanaeFox <36219542+Hoshinonyaruko@users.noreply.github.com>
2024-02-08 18:09:07 +0800

    Beta36 (#84)
    * beta9
    
    * beta11
    
    * beta11
    
    * beta13
    
    * beta12
    
    * beta13
    
    * beta15
    
    * beta16
    
    * beta17
    
    * beta18
    
    * beta19
    
    * beta20
    
    * beta21
    
    * add_playermanager_api
    
    * beta22
    
    * beta23
    
    * beta24
    
    * beta25
    
    * beta26
    
    * beta27
    
    * beta28
    
    * beta28
    
    * beta29
    
    * beta30
    
    * beta31
    
    * beta32
    
    * beta33
    
    * beta34
    
    * beta35
    
    * beta35
    
    * beta36

--- supervisor.go
+++ supervisor.go
@@ -3,17 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"os/exec"
-	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/hoshinonyaruko/palworld-go/config"
-	"github.com/hoshinonyaruko/palworld-go/mod"
 	"github.com/hoshinonyaruko/palworld-go/status"
 	"github.com/hoshinonyaruko/palworld-go/sys"
 )
@@ -44,7 +40,7 @@ func (s *Supervisor) Start() {
 		}
 
 		if !s.isServiceRunning() {
-			s.restartService()
+			sys.RestartService(s.Config)
 		} else {
 			fmt.Println("当前正常运行中~")
 		}
@@ -100,108 +96,3 @@ func (s *Supervisor) isServiceRunning() bool {
 		return true
 	}
 }
-
-func (s *Supervisor) restartService() {
-	var exePath string
-	var args []string
-
-	if runtime.GOOS == "windows" {
-		if s.Config.CommunityServer {
-			exePath = filepath.Join(s.Config.SteamPath, "Steam.exe")
-			args = []string{"-applaunch", "2394010"}
-		} else if s.Config.UseDll {
-			err := mod.CheckAndWriteFiles(filepath.Join(s.Config.GamePath, "Pal", "Binaries", "Win64"))
-			if err != nil {
-				log.Printf("Failed to write files: %v", err)
-				return
-			}
-			exePath = filepath.Join(s.Config.GamePath, "Pal", "Binaries", "Win64", "PalServerInject.exe")
-			args = []string{
-				"-RconEnabled=True",
-				fmt.Sprintf("-AdminPassword=%s", s.Config.WorldSettings.AdminPassword),
-				fmt.Sprintf("-port=%d", s.Config.WorldSettings.PublicPort),
-				fmt.Sprintf("-players=%d", s.Config.WorldSettings.ServerPlayerMaxNum),
-			}
-		} else {
-			exePath = filepath.Join(s.Config.GamePath, "Pal", "Binaries", "Win64", "PalServer-Win64-Test-Cmd.exe")
-			//exePath = "\"" + exePath + "\""
-			args = []string{
-				"Pal",
-				"-RconEnabled=True",
-				fmt.Sprintf("-AdminPassword=%s", s.Config.WorldSettings.AdminPassword),
-				fmt.Sprintf("-port=%d", s.Config.WorldSettings.PublicPort),
-				fmt.Sprintf("-players=%d", s.Config.WorldSettings.ServerPlayerMaxNum),
-			}
-		}
-	} else {
-		// 对于非Windows系统的处理保持不变
-		exePath = filepath.Join(s.Config.GamePath, s.Config.ProcessName+".sh")
-		args = []string{
-			"-RconEnabled=True",
-			fmt.Sprintf("-AdminPassword=%s", s.Config.WorldSettings.AdminPassword),
-			fmt.Sprintf("-port=%d", s.Config.WorldSettings.PublicPort),
-			fmt.Sprintf("-players=%d", s.Config.WorldSettings.ServerPlayerMaxNum),
-		}
-	}
-
-	args = append(args, s.Config.ServerOptions...) // 添加GameWorldSettings参数
-
-	// 执行启动命令
-	log.Printf("启动命令: %s %s", exePath, strings.Join(args, " "))
-	if s.Config.UseDll && runtime.GOOS == "windows" {
-		log.Printf("use bat")
-		sys.RunViaBatch(s.Config, exePath, args)
-		log.Printf("use bat success")
-	} else {
-		cmd := exec.Command(exePath, args...)
-		cmd.Dir = s.Config.GamePath // 设置工作目录为游戏路径
-		if runtime.GOOS == "windows" {
-			// 仅在Windows平台上设置
-			cmd.SysProcAttr = &syscall.SysProcAttr{
-				CreationFlags: 16,
-			}
-		}
-
-		// 启动进程
-		if err := cmd.Start(); err != nil {
-			log.Printf("Failed to restart game server: %v", err)
-		} else {
-			log.Printf("Game server restarted successfully")
-		}
-
-		// 获取并打印 PID
-		log.Printf("Game server started successfully with PID %d", cmd.Process.Pid)
-		status.SetGlobalPid(cmd.Process.Pid)
-	}
-
-}
-
-// func constructGameLaunchArguments(settings *GameWorldSettings) []string {
-// 	var args []string
-
-// 	sValue := reflect.ValueOf(settings).Elem()
-// 	sType := sValue.Type()
-
-// 	for i := 0; i < sType.NumField(); i++ {
-// 		field := sType.Field(i)
-// 		fieldValue := sValue.Field(i)
-
-// 		jsonTag := firstToUpper(strings.Split(field.Tag.Get("json"), ",")[0]) // 获取json标签的第一部分，并将首字母转换为大写
-
-// 		var arg string
-// 		switch fieldValue.Kind() {
-// 		case reflect.String:
-// 			arg = fmt.Sprintf("%s=%s", jsonTag, fieldValue.String())
-// 		case reflect.Float64:
-// 			arg = fmt.Sprintf("%s=%s", jsonTag, strconv.FormatFloat(fieldValue.Float(), 'f', 6, 64))
-// 		case reflect.Int:
-// 			arg = fmt.Sprintf("%s=%d", jsonTag, fieldValue.Int())
-// 		case reflect.Bool:
-// 			arg = fmt.Sprintf("%s=%t", jsonTag, fieldValue.Bool())
-// 		}
-
-// 		args = append(args, arg)
-// 	}
-
-// 	return args
-// }
